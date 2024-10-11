8cae683d92bfb693b5b30eb2d2a17fbac648084f
Ismaeil <ismaeilsaberi@gmail.com>
2024-10-11 08:26:24 -0700

    another simple edit

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -51,7 +51,7 @@ export async function POST(req: Request) {
 
   // Do something with the payload
   // For this guide, you simply log the payload to the console
-  const { id } = evt.data;
+  //   const { id } = evt.data;
   const eventType = evt.type;
   //   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   //   console.log("Webhook body:", body);
