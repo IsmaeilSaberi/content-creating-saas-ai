35b8ac1d01c3c6b5a900a639939c9f10998e243d
Ismaeil <ismaeilsaberi@gmail.com>
2024-10-11 08:20:09 -0700

    adding action for creato or update user

---
+++ utils/db/actions.ts
@@ -0,0 +1,42 @@
+import { db } from "./dbConfig";
+import { eq, sql, desc } from "drizzle-orm";
+import { Users, Subscription, GeneratedContent } from "./schema";
+
+export async function createOrUpdateUser(
+  clerkUserId: string,
+  email: string,
+  name: string
+) {
+  try {
+    const [existingUser] = await db
+      .select()
+      .from(Users)
+      .where(eq(Users.stripeCustomerId, clerkUserId))
+      .limit(1)
+      .execute();
+
+    if (existingUser) {
+      const [updatedUser] = await db
+        .update(Users)
+        .set({ name, email })
+        .where(eq(Users.stripeCustomerId, clerkUserId))
+        .returning()
+        .execute();
+      console.log("Updated user:", updatedUser);
+      return updatedUser;
+    }
+
+    const [newUser] = await db
+      .insert(Users)
+      .values({ email, name, stripeCustomerId: clerkUserId, points: 50 })
+      .returning()
+      .execute();
+
+    console.log("new user created", newUser);
+
+    // send welcome email to new users
+  } catch (error) {
+    console.error("Error creating or updating user:", error);
+    return null;
+  }
+}
