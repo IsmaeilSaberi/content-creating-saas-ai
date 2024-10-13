c928776ffb8405da76e62784d95a4d7030152c20
Ismaeil <ismaeilsaberi@gmail.com>
2024-10-13 09:06:02 -0700

    another simple edit on create-checkout-session route.ts in types

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2024-09-30.acacia",
 });
 export async function POST(req: Request) {
   try {
