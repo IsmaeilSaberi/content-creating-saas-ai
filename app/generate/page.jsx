0914a8875a266cb924bee54432846104cd7115ff
Ismaeil <ismaeilsaberi@gmail.com>
2024-10-13 20:25:08 -0700

    adding shadcn components for dahboard creating(generate content)

---
+++ app/generate/page.jsx
@@ -0,0 +1,22 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Navbar } from "@/components/Navbar";
+
+export default function GenerateContent() {
+  return (
+    <div className="bg-gradient-to-br from-gray-900 to-black min-h-screen text-white">
+      <Navbar />
+      <div className="container mx-auto px-4 mb-8 sm:px-6 lg:px-8 py-8"></div>
+    </div>
+  );
+}
